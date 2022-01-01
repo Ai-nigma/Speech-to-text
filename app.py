f311ab1a137d3a5df3b00a9edbbb078f8ab47c80
AInigma <88995010+Ai-nigma@users.noreply.github.com>
2022-01-01 18:44:54 -0300

    Update app.py

--- app.py
+++ app.py
@@ -64,46 +64,27 @@ st.sidebar.write('### Gracias por confiar en nosotros!')
 '''
 
 # RECORD AUDIO
-import pyaudio
-import wave
+filename = st.text_input("Choose a filename: ")
 
-#DEFINIMOS PARAMETROS
-FORMAT=pyaudio.paInt16
-CHANNELS=2
-RATE=44100
-CHUNK=1024
-archivo="grabacion.wav"
+if st.button(f"Click to Record"):
+    if filename == "":
+        st.warning("Choose a filename.")
+    else:
+        record_state = st.text("Recording...")
+        duration = 5  # seconds
+        fs = 48000
+        myrecording = record(duration, fs)
+        record_state.text(f"Saving sample as {filename}.mp3")
 
-#INICIAMOS "pyaudio"
-audio=pyaudio.PyAudio()
+        path_myrecording = f"./samples/{filename}.mp3"
 
-#INICIAMOS GRABACIÓN
-start_record = st.button('''Click para iniciar grabación''')
+        save_record(path_myrecording, myrecording, fs)
+        record_state.text(f"Done! Saved sample as {filename}.mp3")
 
-if start_record:
-    stream=audio.open(format=FORMAT,channels=CHANNELS,
-                    rate=RATE, input=True,
-                    frames_per_buffer=CHUNK)
-    st.write('Grabando')
-    frames=[]
+        st.audio(read_audio(path_myrecording))
 
-    data=stream.read(CHUNK)
-    frames.append(data)
-    
-    finish_record = st.button('Click para detener grabación')
-    if finish_record:
-#DETENEMOS GRABACIÓN
-        stream.stop_stream()
-        stream.close()
-        audio.terminate()
-
-#CREAMOS/GUARDAMOS EL ARCHIVO DE AUDIO
-        waveFile = wave.open(archivo, 'wb')
-        waveFile.setnchannels(CHANNELS)
-        waveFile.setsampwidth(audio.get_sample_size(FORMAT))
-        waveFile.setframerate(RATE)
-        waveFile.writeframes(b''.join(frames))
-        waveFile.close()
+        fig = create_spectrogram(path_myrecording)
+        st.pyplot(fig)
 # END RECORD
 
 
