e338a4e8095a38e25d6b0f1d16f544e3797ea090
AInigma <88995010+Ai-nigma@users.noreply.github.com>
2022-01-01 19:06:29 -0300

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,26 @@ import soundfile as sf
 import os
 import librosa
 import glob
-from helper import record, record_audio, save_record
+
+import sounddevice as sd
+import wavio
+
+def read_audio(file):
+    with open(file, "rb") as audio_file:
+        audio_bytes = audio_file.read()
+    return audio_bytes
+
+def record(duration=5, fs=48000):
+    sd.default.samplerate = fs
+    sd.default.channels = 1
+    myrecording = sd.rec(int(duration * fs))
+    sd.wait(duration)
+    return myrecording
+
+def save_record(path_myrecording, myrecording, fs):
+    wavio.write(path_myrecording, myrecording, fs, sampwidth=2)
+    return None
+
 
 
 def word2num (num):
