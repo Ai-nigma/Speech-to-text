e8e7c0c32674e5b378be7c55d6687ddd10947190
AInigma <88995010+Ai-nigma@users.noreply.github.com>
2022-01-01 21:03:19 -0300

    Update app.py

--- app.py
+++ app.py
@@ -61,35 +61,68 @@ def word2num (num):
     except:
         return num
     '''
-class OpenCVEdgeProcessor(VideoProcessorBase):
-    def recv(self, frame: av.VideoFrame) -> av.VideoFrame:
-        img = frame.to_ndarray(format="bgr24")
+import queue
 
-            # perform edge detection
-        img = cv2.cvtColor(cv2.Canny(img, 100, 200), cv2.COLOR_GRAY2BGR)
+import pydub
 
-        return av.VideoFrame.from_ndarray(img, format="bgr24")
+import streamlit as st
+from streamlit_webrtc import webrtc_streamer, WebRtcMode, ClientSettings
 
-def in_recorder_factory() -> MediaRecorder:
-    return MediaRecorder(
-        "input.flv", format="flv"
-    )  # HLS does not work. See https://github.com/aiortc/aiortc/issues/331
 
-def out_recorder_factory() -> MediaRecorder:
-    return MediaRecorder("output.flv", format="flv")
-
-webrtc_streamer(
-        key="loopback",
-        mode=WebRtcMode.SENDRECV,
-        rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
-        media_stream_constraints={
-            "video": True,
-            "audio": True,
-        },
-        video_processor_factory=OpenCVEdgeProcessor,
-        in_recorder_factory=in_recorder_factory,
-        out_recorder_factory=out_recorder_factory,
+def main():
+    webrtc_ctx = webrtc_streamer(
+        key="sendonly-audio",
+        mode=WebRtcMode.SENDONLY,
+        audio_receiver_size=256,
+        client_settings=ClientSettings(
+            rtc_configuration={
+                "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
+            },
+            media_stream_constraints={
+                "audio": True,
+            },
+        ),
     )
+
+    if "audio_buffer" not in st.session_state:
+        st.session_state["audio_buffer"] = pydub.AudioSegment.empty()
+
+    status_indicator = st.empty()
+
+    while True:
+        if webrtc_ctx.audio_receiver:
+            try:
+                audio_frames = webrtc_ctx.audio_receiver.get_frames(timeout=1)
+            except queue.Empty:
+                status_indicator.write("No frame arrived.")
+                continue
+
+            status_indicator.write("Running. Say something!")
+
+            sound_chunk = pydub.AudioSegment.empty()
+            for audio_frame in audio_frames:
+                sound = pydub.AudioSegment(
+                    data=audio_frame.to_ndarray().tobytes(),
+                    sample_width=audio_frame.format.bytes,
+                    frame_rate=audio_frame.sample_rate,
+                    channels=len(audio_frame.layout.channels),
+                )
+                sound_chunk += sound
+
+            if len(sound_chunk) > 0:
+                st.session_state["audio_buffer"] += sound_chunk
+        else:
+            status_indicator.write("AudioReciver is not set. Abort.")
+            break
+
+    audio_buffer = st.session_state["audio_buffer"]
+
+    if not webrtc_ctx.state.playing and len(audio_buffer) > 0:
+        st.info("Writing wav to disk")
+        audio_buffer.export("temp.wav", format="wav")
+
+        # Reset
+        st.session_state["audio_buffer"] = pydub.AudioSegment.empty()
 
 
 '''
