299a13adf28c3437d7aec0e99ea38715d9eb8642
AInigma <88995010+Ai-nigma@users.noreply.github.com>
2022-01-01 21:22:00 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8,14 +8,22 @@ import base64
 import io
 
 #-------------------------
-
-import queue
-
-import pydub
-import av
-import cv2
+from streamlit_webrtc import (
+    webrtc_streamer,
+    WebRtcMode,
+    WebRtcStreamerContext,
+)
 from aiortc.contrib.media import MediaRecorder
-from streamlit_webrtc import webrtc_streamer, WebRtcMode, ClientSettings
+import soundfile as sf
+import numpy as np
+import matplotlib.pyplot as plt
+import queue
+from pathlib import Path
+import time
+import pydub
+
+# from streamlit_lottie import st_lottie
+import json
 
 file = 'favicon.png'
 favicon = Image.open(file)
@@ -61,24 +69,121 @@ def word2num (num):
     except:
         return num
     '''
-def main():
-    def recorder_factory():
-        return MediaRecorder("record.wav")
 
-    webrtc_streamer(
+
+# file_ = '16581-audio.json'
+# with open(file_, 'r', encoding='utf-8') as f:
+#     lottie_json = json.load(f)
+
+TMP_DIR = Path('temp')
+if not TMP_DIR.exists():
+    TMP_DIR.mkdir(exist_ok=True, parents=True)
+
+MEDIA_STREAM_CONSTRAINTS = {
+    "video": False,
+    "audio": {
+        # these setting doesn't work
+        # "sampleRate": 48000,
+        # "sampleSize": 16,
+        # "channelCount": 1,
+        "echoCancellation": False,  # don't turn on else it would reduce wav quality
+        "noiseSuppression": True,
+        "autoGainControl": True,
+    },
+}
+
+
+def aiortc_audio_recorder(wavpath):
+    def recorder_factory():
+        return MediaRecorder(wavpath)
+
+    webrtc_ctx: WebRtcStreamerContext = webrtc_streamer(
+        key="sendonly-audio",
+        # mode=WebRtcMode.SENDONLY,
+        mode=WebRtcMode.SENDRECV,
+        in_recorder_factory=recorder_factory,
+        media_stream_constraints=MEDIA_STREAM_CONSTRAINTS,
+    )
+
+
+def save_frames_from_audio_receiver(wavpath):
+    webrtc_ctx = webrtc_streamer(
         key="sendonly-audio",
         mode=WebRtcMode.SENDONLY,
-        in_recorder_factory=recorder_factory,
-        client_settings=ClientSettings(
-            rtc_configuration={
-                "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
-            },
-            media_stream_constraints={
-                "audio": True,
-                "video": False,
-            },
-        ),
+        media_stream_constraints=MEDIA_STREAM_CONSTRAINTS,
     )
+
+    if "audio_buffer" not in st.session_state:
+        st.session_state["audio_buffer"] = pydub.AudioSegment.empty()
+
+    status_indicator = st.empty()
+    lottie = False
+    while True:
+        if webrtc_ctx.audio_receiver:
+            try:
+                audio_frames = webrtc_ctx.audio_receiver.get_frames(timeout=1)
+            except queue.Empty:
+                status_indicator.info("No frame arrived.")
+                continue
+
+            # if not lottie:  # voice gif
+            #     st_lottie(lottie_json, height=80)
+            #     lottie = True
+
+            for i, audio_frame in enumerate(audio_frames):
+                sound = pydub.AudioSegment(
+                    data=audio_frame.to_ndarray().tobytes(),
+                    sample_width=audio_frame.format.bytes,
+                    frame_rate=audio_frame.sample_rate,
+                    channels=len(audio_frame.layout.channels),
+                )
+                # st.markdown(f'{len(audio_frame.layout.channels)}, {audio_frame.format.bytes}, {audio_frame.sample_rate}')
+                # 2, 2, 48000
+                st.session_state["audio_buffer"] += sound
+        else:
+            lottie = True
+            break
+
+    audio_buffer = st.session_state["audio_buffer"]
+
+    if not webrtc_ctx.state.playing and len(audio_buffer) > 0:
+        audio_buffer.export(wavpath, format="wav")
+        st.session_state["audio_buffer"] = pydub.AudioSegment.empty()
+
+
+def display_wavfile(wavpath):
+    audio_bytes = open(wavpath, 'rb').read()
+    file_type = Path(wavpath).suffix
+    st.audio(audio_bytes, format=f'audio/{file_type}', start_time=0)
+
+
+def plot_wav(wavpath):
+    audio, sr = sf.read(str(wavpath))
+    fig = plt.figure()
+    plt.plot(audio)
+    plt.xticks(
+        np.arange(0, audio.shape[0], sr / 2), np.arange(0, audio.shape[0] / sr, 0.5)
+    )
+    plt.xlabel('time')
+    st.pyplot(fig)
+
+
+def main():
+    st.markdown('# recorder')
+    if "wavpath" not in st.session_state:
+        cur_time = time.strftime("%Y-%m-%d_%H:%M:%S", time.localtime())
+        tmp_wavpath = TMP_DIR / f'{cur_time}.wav'
+        st.session_state["wavpath"] = str(tmp_wavpath)
+
+    wavpath = st.session_state["wavpath"]
+
+    aiortc_audio_recorder(wavpath)  # first way
+    # save_frames_from_audio_receiver(wavpath)  # second way
+
+    if Path(wavpath).exists():
+        st.markdown(wavpath)
+        display_wavfile(wavpath)
+        plot_wav(wavpath)
         
 main()
 '''
